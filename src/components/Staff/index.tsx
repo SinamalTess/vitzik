92e993e76f5627aa1351ff69abab9a0bce9cf5f3
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-05-28 23:37:00 +0200

    feat: add bass clef staff

--- src/components/Staff/index.tsx
+++ src/components/Staff/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Vex from 'vexflow'
 import StaveNote = Vex.Flow.StaveNote
 import Formatter = Vex.Flow.Formatter
+import { KEYS } from '../../utils/const/keys'
 
 interface StaffProps {
     note: string | null
@@ -19,36 +20,53 @@ export function Staff({ note }: StaffProps) {
                 Renderer.Backends.SVG
             )
 
-            renderer.resize(400, 200)
+            renderer.resize(800, 400)
 
             const context = renderer.getContext()
 
             context.scale(2, 2)
             context.setFillStyle('white')
 
-            const stave = new Stave(0, 0, 150)
+            const trebleStaff = new Stave(0, 0, 400)
+            const bassStaff = new Stave(0, 75, 400)
 
-            stave.addClef('treble')
-            stave.setContext(context).draw()
+            trebleStaff.addClef('treble')
+            bassStaff.addClef('bass')
 
-            const noteFormatted = note
-                ? note.substring(0, 1).toLowerCase() +
-                  '/' +
-                  note.substring(1, 2).toLowerCase()
-                : 'c/4'
+            const lineLeft = new Vex.Flow.StaveConnector(
+                trebleStaff,
+                bassStaff
+            ).setType(1)
+            const lineRight = new Vex.Flow.StaveConnector(
+                trebleStaff,
+                bassStaff
+            ).setType(6)
 
-            const notes = [
-                new StaveNote({
-                    keys: [noteFormatted],
-                    duration: 'q',
-                }),
-            ]
+            trebleStaff.setContext(context).draw()
+            bassStaff.setContext(context).draw()
+            lineLeft.setContext(context).draw()
+            lineRight.setContext(context).draw()
 
-            const voice = new Vex.Flow.Voice({ num_beats: 1, beat_value: 4 })
+            if (note) {
+                const isTopNote = KEYS.alphabetical.indexOf(note) >= 39 // middle C4 has index of 39 in KEYS
+                const noteFormatted =
+                    note.substring(0, 1) + '/' + note.substring(1, 2)
+                const voice = new Vex.Flow.Voice({
+                    num_beats: 1,
+                    beat_value: 4,
+                })
+                const notes = [
+                    new StaveNote({
+                        keys: [noteFormatted],
+                        duration: 'q',
+                        clef: isTopNote ? 'treble' : 'bass',
+                    }),
+                ]
 
-            voice.addTickables(notes)
-            new Formatter().joinVoices([voice]).format([voice], 50)
-            voice.draw(context, stave)
+                voice.addTickables(notes)
+                new Formatter().joinVoices([voice]).format([voice], 200)
+                voice.draw(context, isTopNote ? trebleStaff : bassStaff)
+            }
         }
 
         // Cleaning function
@@ -61,6 +79,6 @@ export function Staff({ note }: StaffProps) {
 
     return (
         // @ts-ignore
-        <div ref={musicSheet}></div>
+        <div id="musicSheet" ref={musicSheet}></div>
     )
 }
