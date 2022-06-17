a2c430a49d8f4f4b9c79b76af2d8889ee8dc45df
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-06-17 23:48:44 +0200

    perf(InstrumentSelector): memoize InstrumentSelector component

--- src/components/InstrumentSelector.tsx
+++ src/components/InstrumentSelector.tsx
@@ -7,7 +7,14 @@ interface InstrumentSelectorProps {
     onChange: (instrument: Instrument) => void
 }
 
-export function InstrumentSelector({ onChange }: InstrumentSelectorProps) {
+/*
+    This component shows a large list that takes a long time to paint.
+    React.memo was used to avoid unnecessary re-renders
+*/
+
+export const InstrumentSelector = React.memo(function InstrumentSelector({
+    onChange,
+}: InstrumentSelectorProps) {
     return (
         <Select name="instruments" onChange={(event) => onChange(event.target.value)}>
             {MIDI_INSTRUMENTS.map((instrument) => (
@@ -17,4 +24,4 @@ export function InstrumentSelector({ onChange }: InstrumentSelectorProps) {
             ))}
         </Select>
     )
-}
+})
