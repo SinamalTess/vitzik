6fdcfed31998ebc1ba8691deb9f7dfc956930a6b
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-06-20 23:22:09 +0200

    test(ErrorBoundary): add tests for ErrorBoundary component

---
+++ src/components/generics/ErrorBoundary.test.tsx
@@ -0,0 +1,28 @@
+import { screen, render } from '@testing-library/react'
+import React from 'react'
+import { ErrorBoundary } from './ErrorBoundary'
+
+function BrokenComponent() {
+    throw new Error('this component is broken')
+    // eslint-disable-next-line no-unreachable
+    return <h1>This won't show</h1>
+}
+
+describe('ErrorBoundary', () => {
+    beforeEach(() => {
+        jest.spyOn(console, 'error').mockImplementation(() => {})
+    })
+
+    test('catch and display errors', () => {
+        render(
+            <ErrorBoundary>
+                <BrokenComponent />
+            </ErrorBoundary>
+        )
+
+        const text = screen.getByText(/something went wrong/i)
+        const error = screen.getByText(/this component is broken/i)
+        expect(text).toBeVisible()
+        expect(error).toBeVisible()
+    })
+})
