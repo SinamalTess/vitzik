f27931fe720a55aea3d83deeeb548a1f2a9c206c
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-06-21 10:38:41 +0200

    test(MusicSystemSelector): add test for MusicSystemSelector component

---
+++ src/components/MusicSystemSelector.test.tsx
@@ -0,0 +1,41 @@
+import { screen, render } from '@testing-library/react'
+import React from 'react'
+import { MusicSystemSelector } from './MusicSystemSelector'
+import userEvent from '@testing-library/user-event'
+
+describe('MusicSystemSelector', () => {
+    const onChange = jest.fn()
+
+    test('calls onChange() callback with "syllabic" mode', () => {
+        render(
+            <MusicSystemSelector
+                musicSystem={'alphabetical'}
+                onChange={onChange}
+            ></MusicSystemSelector>
+        )
+        const button = screen.getByText(/Si/i)
+        userEvent.click(button)
+        expect(onChange).toHaveBeenCalledWith('syllabic')
+    })
+
+    test('calls onChange() callback with "german" mode', () => {
+        render(
+            <MusicSystemSelector
+                musicSystem={'alphabetical'}
+                onChange={onChange}
+            ></MusicSystemSelector>
+        )
+        const button = screen.getByText(/H/i)
+        userEvent.click(button)
+        expect(onChange).toHaveBeenCalledWith('german')
+    })
+
+    test('calls onChange() callback with "alphabetical" mode', () => {
+        render(
+            <MusicSystemSelector musicSystem={'german'} onChange={onChange}></MusicSystemSelector>
+        )
+        const button = screen.getByText(/B/i)
+        userEvent.click(button)
+        expect(onChange).toHaveBeenCalledWith('alphabetical')
+    })
+})
