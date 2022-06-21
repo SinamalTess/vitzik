9398b4f43086e20df631f91eb1fb45fcd2eaee5d
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-06-21 23:42:45 +0200

    refactor: simplify midi.ts logic

--- src/utils/midi.ts
+++ src/utils/midi.ts
@@ -16,47 +16,65 @@ export const isNoteOnEvent = (note: TMidiEvent): note is IMidiNoteOnEvent => 'no
 export const isNoteOffEvent = (note: TMidiEvent): note is IMidiNoteOffEvent => 'noteOff' in note
 export const isProgramChangeEvent = (event: TMidiEvent): event is IMidiProgramChangeEvent =>
     'programChange' in event
+export const isSetTempoEvent = (event: TMidiEvent): event is IMidiSetTempoEvent =>
+    'setTempo' in event
 
 const programNumberToInstrument = (programNumber: number): Instrument =>
     MIDI_INSTRUMENTS[programNumber]
 
+type EventType = 'setTempo' | 'noteOn' | 'noteOff'
+
 export function getTicksPerBeat(midiJson: IMidiFile) {
     const { division } = midiJson
     const value = Math.sign(division)
-    const isTickPerBeat = value === 0 || value === 1
+    const isValuePositive = value === 0 || value === 1
 
-    if (isTickPerBeat) {
-        console.log(
-            `The file is using ticks per beat, there are ${division} ticks per beat (quarter note)`
-        )
+    if (isValuePositive) {
+        // The "division" is equal to the ticks per beat (beat = quarter note)
+        return division
     } else {
-        console.log(`The file is using SMPTE units`) // TODO: complete this section
+        // The file is using SMPTE units (ticks per frame)
+        throw new Error(
+            'Congratulations you have found a SMPTE formatted midi file, a rare gem, I have no idea how to process it...yet'
+        )
     }
-
-    return division
 }
 
-export function getMsPerBeat(midiJson: IMidiFile): number {
+function getAllEvents(track: TMidiEvent[], eventType: EventType) {
+    switch (eventType) {
+        case 'setTempo':
+            return track.filter((event) => isSetTempoEvent(event))
+        case 'noteOff':
+            return track.filter((event) => isNoteOffEvent(event))
+        case 'noteOn':
+            return track.filter((event) => isNoteOnEvent(event))
+        default:
+            return []
+    }
+}
+
+function getLastTempoValue(track: TMidiEvent[]) {
+    const setTempoEvents = getAllEvents(track, 'setTempo') as IMidiSetTempoEvent[]
+
+    if (setTempoEvents.length > 1) {
+        const lastSetTempo = setTempoEvents[setTempoEvents.length - 1]
+        const { microsecondsPerQuarter } = lastSetTempo.setTempo
+        return Math.round(microsecondsPerQuarter / 1000)
+    }
+}
+
+function getMsPerBeat(midiJson: IMidiFile): number {
     const format = getFormat(midiJson)
     const defaultTempo = 60000 / 120 // 120 beats per minute is the default value
+    const firstTrack = midiJson.tracks[0] // TODO: a tempo can be changed in the middle of the file
     switch (format) {
         case 0: // TODO: complete this section
             return defaultTempo
         case 1:
-            const setTempoEvents = midiJson.tracks[0].filter((event) =>
-                event.hasOwnProperty('setTempo')
-            ) as IMidiSetTempoEvent[]
-
-            if (setTempoEvents.length > 1) {
-                const lastSetTempo = setTempoEvents[setTempoEvents.length - 1]
-                const { microsecondsPerQuarter } = lastSetTempo.setTempo
-                const msPerBeat = Math.round(microsecondsPerQuarter / 1000)
-
-                console.log(`There are ${msPerBeat} milliseconds per quarter note (beat)`)
-
+            const msPerBeat = getLastTempoValue(firstTrack)
+            if (msPerBeat) {
                 return msPerBeat
             }
-
             return defaultTempo
 
         case 2: // TODO: complete this section
