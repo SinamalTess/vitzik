aafe2818bd5adbce14604477fc7c7882c1a0a649
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-06-24 00:48:16 +0200

    fix(Button): typo on disabled prop

--- src/components/generics/Button.tsx
+++ src/components/generics/Button.tsx
@@ -4,31 +4,31 @@ import { Icon, IconName } from './Icon'
 import { CSSSpacingSize, CSSColor } from '../../types'
 
 export interface ButtonProps {
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
-    disable?: boolean
+    disabled?: boolean
     active?: boolean
     icon?: IconName
     children?: ReactNode
     size?: CSSSpacingSize
     color?: CSSColor
     outline?: boolean
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 export function Button({
-    onClick = () => {},
-    disable = false,
+    disabled = false,
     active = false,
     icon,
     children,
     size = 'md',
     outline = false,
+    onClick = () => {},
 }: ButtonProps) {
     const className = `btn btn--${size} pd-${size} ${active ? 'btn--active' : ''} ${
         outline ? 'btn--outline' : ''
     }`
 
     return (
-        <button onClick={onClick} disabled={disable} className={className}>
+        <button disabled={disabled} className={className} onClick={onClick}>
             {icon ? <Icon name={icon} size={size} /> : null}
             {children}
         </button>
