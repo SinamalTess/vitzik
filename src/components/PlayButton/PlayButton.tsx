99f28701d58acf5518f206223c204ddfc2318415
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-07-19 20:54:28 +0200

    feat: add optional size to audio player control buttons

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Button } from '../_presentational/Button'
 import './PlayButton.scss'
 import clsx from 'clsx'
+import { CSSSpacingSize } from '../_presentational/types'
 
 interface PlayerControllerProps {
+    size?: CSSSpacingSize
     isPlaying: boolean
     onClick: () => void
 }
 
 const BASE_CLASS = 'play-button'
 
-export function PlayButton({ onClick, isPlaying }: PlayerControllerProps) {
+export function PlayButton({ size, isPlaying, onClick }: PlayerControllerProps) {
     const classNames = clsx(BASE_CLASS, {
         [`${BASE_CLASS}--pause`]: isPlaying,
     })
@@ -20,6 +22,7 @@ export function PlayButton({ onClick, isPlaying }: PlayerControllerProps) {
             onClick={onClick}
             variant={'link'}
             aria-label={isPlaying ? 'play button' : 'paused button'}
+            size={size}
         >
             <span className={classNames} aria-hidden="true">
                 <span className={`${BASE_CLASS}__half ${BASE_CLASS}__half--before`}></span>
--- src/components/SoundButton/SoundButton.tsx
+++ src/components/SoundButton/SoundButton.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Button } from '../_presentational/Button'
 import './SoundButton.scss'
 import clsx from 'clsx'
+import { CSSSpacingSize } from '../_presentational/types'
 
 interface SoundButtonProps {
+    size?: CSSSpacingSize
     isMute: boolean
     onMute: (isMute: boolean) => void
 }
 
 const BASE_CLASS = 'volume-icon'
 
-export function SoundButton({ isMute, onMute }: SoundButtonProps) {
+export function SoundButton({ size, isMute, onMute }: SoundButtonProps) {
     function handleClick() {
         onMute(!isMute)
     }
@@ -23,6 +25,7 @@ export function SoundButton({ isMute, onMute }: SoundButtonProps) {
             variant="link"
             className={classNames}
             aria-label="volume button"
+            size={size}
         >
             <svg viewBox="0 0 108 96" aria-hidden="true">
                 <path d="M7 28h28L59 8v80L35 68H7a4 4 0 01-4-4V32a4 4 0 014-4z" />
