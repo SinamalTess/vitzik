91175e51af86bbe20849f53156cdac0545f9984d
Tess Sinamal <tess.sinamal@gmail.com>
2024-02-03 12:15:31 +0100

    refactor: optimize clamp function

--- packages/vitzik-app/src/utils/maths.ts
+++ packages/vitzik-app/src/utils/maths.ts
@@ -2,6 +2,5 @@ export const isEven = (number: number) => number % 2 === 0
 
 export const isPositive = (number: number) => Math.sign(number) >= 0 && !Object.is(number, -0)
 
-export const clamp = (value: number, min: number, max: number) => {
-    return value > max ? max : value < min ? min : value;
-}
+export const clamp = (value: number, min: number, max: number) =>
+    Math.min(Math.max(value, min), max)
